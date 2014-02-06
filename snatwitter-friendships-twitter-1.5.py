00a75a1dd798e84bbbbada273216df9db459afae
Massimo Menichinelli <info@openp2pdesign.org>
2014-02-06 15:19:43 +0100

    Improve 1.5 degree search, now 2

--- snatwitter-friendships-twitter-1.5.py
+++ snatwitter-friendships-twitter-1.5.py
@@ -45,6 +45,7 @@ def load_connections(user_list,option):
 		connections[i] = []
 
 		while cursor != "0":
+			# API: https://dev.twitter.com/docs/api/1.1/get/friends/ids
 			try:
 				if option == "followers":
 					query = twitter.followers.ids(user_id=i,count=5000,cursor=cursor)
@@ -77,9 +78,11 @@ def load_connections(user_list,option):
 				elif "Not authorized" in str(e):				
 					print "There were some errors with user",i,"... most likely it is a protected user"
 					cursor = "0"
+					errors += 1
 				else:
 					print "Some error happened with user",i
 					cursor = "0"
+					errors += 1
 	return connections
 
 # Get them from http://dev.twitter.com
@@ -87,6 +90,7 @@ OAUTH_TOKEN = "Insert here"
 OAUTH_SECRET = "Insert here"
 CONSUMER_KEY = "Insert here"
 CONSUMER_SECRET = "Insert here"
+
 
 # Log in
 auth = OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
@@ -100,10 +104,11 @@ first_followers = load_connections(starting_user, "followers")
 first_friends = load_connections(starting_user, "friends")
 
 second_followers_followers = load_connections(first_followers[starting_user[0]], "followers")
-second_followers_friends = load_connections(first_followers[starting_user[0]], "friends")
-second_friends_followers = load_connections(first_friends[starting_user[0]], "followers")
-second_friends_friends = load_connections(first_friends[starting_user[0]], "friends")
+#second_followers_friends = load_connections(first_followers[starting_user[0]], "friends")
+#second_friends_followers = load_connections(first_friends[starting_user[0]], "followers")
+#second_friends_friends = load_connections(first_friends[starting_user[0]], "friends")
 
+# Add edges...
 print ""
 print "Checking 2 degree connections..."
 
@@ -119,56 +124,60 @@ for f in second_followers_followers:
 	for k in second_followers_followers[f]:
 		graph.add_edge(k,f)
 
-for f in second_followers_friends:
-	for k in second_followers_friends[f]:
-		graph.add_edge(f,k)
+#for f in second_followers_friends:
+#	for k in second_followers_friends[f]:
+#		graph.add_edge(f,k)
 
-for f in second_friends_followers:
-	for k in second_friends_followers[f]:
-		graph.add_edge(k,f)
+#for f in second_friends_followers:
+#	for k in second_friends_followers[f]:
+#		graph.add_edge(k,f)
 
-for f in second_friends_friends:
-	for k in second_friends_friends[f]:
-		graph.add_edge(f,k)
+#for f in second_friends_friends:
+#	for k in second_friends_friends[f]:
+#		graph.add_edge(f,k)
 
-
+# Prepare 100 ids lists for converting id to screen names
 mapping = {}
+lista = {}
+position = 0
+hundreds = 0
+lista[hundreds] = []
 for k in graph.nodes():
-	print "node:",k
-	#query = twitter.users.lookup(user_id)
-	#mapping[K] = 
+	if position == 100:
+		hundreds += 1
+		position = 0
+		lista[hundreds] = []
+	lista[hundreds].append(k)
+	position += 1
 
+# Convert id to screen names
+for k in lista:
+	try:
+		# API: https://dev.twitter.com/docs/api/1.1/get/users/lookup
+		query = twitter.users.lookup(user_id=lista[k])
+	except Exception,e:
+		if "Rate limit exceeded" in str(e):
+			print "Rate exceeded... waiting 15 minutes before retrying"
+		
+			# Countdown http://stackoverflow.com/questions/3249524/print-in-one-line-dynamically-python
+			for k in range(1,60*15):
+				remaining = 60*15 - k
+				sys.stdout.write("\r%d seconds remaining   " % remaining)
+				sys.stdout.flush()
+				sleep(1)
+			sys.stdout.write("\n")
+			
+			query = twitter.users.lookup(user_id=lista[k])
+	for i in query:
+		mapping[i["id"]] = i["screen_name"]
+		print i["id"],"=", i["screen_name"]
 
-print ""
-print "Checking 1.5 degree connections..."
-for i in first_followers[username]:
-	for k in first_friends[username]:
-		connection = first_followers[username][i]+", "+first_friends[username][k]
-		try:		
-			# API: https://dev.twitter.com/docs/api/1.1/get/friendships/show
-			query = twitter.friendships.show(source_screen_name=first_followers[username][i], target_screen_name=first_friends[username][k])
-		except Exception,e:
-			if "Rate limit exceeded" in str(e):
-				print "Rate exceeded... waiting 15 minutes before retrying"
-				# Countdown http://stackoverflow.com/questions/3249524/print-in-one-line-dynamically-python
-				for k in range(1,60*15):
-					remaining = 60*15 - k
-					sys.stdout.write("\r%d seconds remaining   " % remaining)
-					sys.stdout.flush()
-					sleep(1)
-				sys.stdout.write("\n")	
-				query = twitter.friendships.show(source_screen_name=first_followers[username][i], target_screen_name=first_friends[username][k])
-		if query['relationship']['source']['following']:
-			graph.add_edge(first_followers[username][i],first_friends[username][k])
-			print "-",first_followers[username][i],"follows",first_friends[username][k]
-		if query['relationship']['target']['following']:
-			graph.add_edge(first_friends[username][k],first_followers[username][i])
-			print "-",first_friends[username][k],"follows",first_followers[username][i]
+# Swap node names from ids to screen names
+graph_screen_names = nx.relabel_nodes(graph,mapping)
 	
-
 # Save graph
 print ""
 print "The personal profile was analyzed succesfully.",errors,"errors were encountered."
 print ""
 print "Saving the file as "+username+"-twitter-personal-network-1.5-degree.gexf..."
-nx.write_gexf(graph, username+"-twitter-personal-network-1.5-degree.gexf")
+nx.write_gexf(graph_screen_names, username+"-twitter-personal-network-1.5-degree.gexf")
