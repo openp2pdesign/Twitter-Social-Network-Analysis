a840750e05bab79e68b1b1c07550496e806da5c3
Massimo Menichinelli <info@openp2pdesign.org>
2014-02-07 22:27:57 +0100

    Implement load members of a list

--- snatwitter-list-friendships.py
+++ snatwitter-list-friendships.py
@@ -23,13 +23,13 @@ import os
 os.system('cls' if os.name=='nt' else 'clear')
 
 graph=nx.DiGraph()
-username = "openmetadesign"
+username = "openp2pdesign"
 errors = 0
 connections = {}
 
 print ""
 print "....................................................."
-print "FRIENDSHIPS OF A TWITTER USER:",username
+print "FRIENDSHIPS WITHIN A TWITTER LIST OF:",username
 print ""	
 
 def load_connections(user_list,option):
@@ -91,14 +91,56 @@ OAUTH_SECRET = "Insert here"
 CONSUMER_KEY = "Insert here"
 CONSUMER_SECRET = "Insert here"
 
+
 # Log in
 auth = OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 twitter = Twitter(auth = auth)
 
-# Load first degree of followers and friends
-query = twitter.users.lookup(screen_name=username)
-starting_user = []
-starting_user.append(query[0]["id"])
+# Load lists of the user
+# API: https://dev.twitter.com/docs/api/1.1/get/lists/list
+query = twitter.lists.list(screen_name=username)
+for i in query:
+	print "-",i["name"],"list (id =",i["id"],") has",i["member_count"],"users."
+
+# Choose a list
+print ""
+choice = raw_input("Enter the id of one of these lists: ")
+
+# Load list members
+cursor = -1
+members = {}
+print ""
+print "Members of the list:"
+while cursor != 0:
+	try:
+		# API: https://dev.twitter.com/docs/api/1.1/get/lists/members
+		query = twitter.lists.members(list_id=choice, cursor=cursor)
+		cursor = query["next_cursor"]
+	except Exception,e:
+		if "Rate limit exceeded" in str(e):
+			print "Rate exceeded... waiting 15 minutes before retrying"
+		
+			# Countdown http://stackoverflow.com/questions/3249524/print-in-one-line-dynamically-python
+			for k in range(1,60*15):
+				remaining = 60*15 - k
+				sys.stdout.write("\r%d seconds remaining   " % remaining)
+				sys.stdout.flush()
+				sleep(1)
+			sys.stdout.write("\n")
+			
+			query = twitter.lists.members(list_id=choice, cursor=cursor)
+			cursor = query["next_cursor"]
+		
+	for i in query["users"]:
+		print "-",i["screen_name"],"(id =",i["id"],")"
+		members[i["id"]] = i["screen_name"]
+
+for i in members:
+	print i, members[i]
+
+exit()
+
+# Load connections of each member
 print ""
 print "Loading followers..."
 first_followers = load_connections(starting_user, "followers")
